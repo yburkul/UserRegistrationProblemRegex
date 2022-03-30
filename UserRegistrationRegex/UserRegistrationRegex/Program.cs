547c9bdeec5e28b90401f8c5c47af07379dfd392
yburkul <yburkul123@gmail.com>
2022-03-30 23:30:25 +0530

    Added:First Name

---
+++ UserRegistrationRegex/UserRegistrationRegex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrations userRegistration = new UserRegistrations();
+            Console.WriteLine("Enter a First Name");
+            string FirstName = Console.ReadLine();
+            userRegistration.validfirstname(FirstName);
+        }
+    }
+}
---
+++ UserRegistrationRegex/UserRegistrationRegex/UserRegistrations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    public class UserRegistrations
+    {
+        public Regex FirstNameRegex = new Regex(@"^[A-Z][A-Za-z]{2,}$");
+        public void validfirstname(string FirstName)
+        {
+            Console.WriteLine("First name: " + FirstName);
+            if (FirstNameRegex.IsMatch(FirstName))
+            {
+                Console.WriteLine("First name is valid");
+
+            }
+            else
+            {
+                Console.WriteLine("First name is not valid");
+            }
+        }
+    }
+}
